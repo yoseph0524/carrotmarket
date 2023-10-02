1e2cf958bdaeb62fb9273df3be29eaf84f2126d8
yoseph0524 <yoseph0524@g.ucla.edu>
2023-10-02 06:36:28 -0700

    register

---
+++ pages/forms.tsx
@@ -0,0 +1,23 @@
+import { useForm } from "react-hook-form";
+
+export default function Forms() {
+  const { register, watch } = useForm();
+  return (
+    <form>
+      <input
+        {...register("username")}
+        type="text"
+        placeholder="Username"
+        required
+      />
+      <input {...register("email")} type="email" placeholder="Email" required />
+      <input
+        {...register("password")}
+        type="password"
+        placeholder="Password"
+        required
+      />
+      <input type="submit" value="Create Account" />
+    </form>
+  );
+}
