affba466b0eb3502929e9e85fe302c66fb97f60a
yoseph0524 <yoseph0524@g.ucla.edu>
2023-10-01 23:49:44 -0700

    prisma

---
+++ pages/api/client-test.tsx
@@ -0,0 +1,18 @@
+import client from "../../libs/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await client.user.create({
+    data: {
+      email: "hi",
+      name: "hi",
+    },
+  });
+  res.json({
+    ok: true,
+    data: "xx",
+  });
+}
