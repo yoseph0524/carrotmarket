f54ee6b3d509f676bd3b1a5bbe3548fa4a9b7059
yoseph0524 <yoseph0524@g.ucla.edu>
2023-10-02 11:41:51 -0700

    validation

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,21 +1,45 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+// Better validation
+// Better Erros (set, clear, display)
+// Have control over inputs
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+}
 
 export default function Forms() {
-  const { register, watch } = useForm();
+  const { register, handleSubmit } = useForm<LoginForm>();
+  const onValid = (data: LoginForm) => {
+    console.log("im valid bby");
+  };
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
       <input
-        {...register("username")}
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars.",
+            value: 5,
+          },
+        })}
         type="text"
         placeholder="Username"
-        required
       />
-      <input {...register("email")} type="email" placeholder="Email" required />
       <input
-        {...register("password")}
+        {...register("email", { required: "Email is required" })}
+        type="email"
+        placeholder="Email"
+      />
+      <input
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
-        required
       />
       <input type="submit" value="Create Account" />
     </form>
