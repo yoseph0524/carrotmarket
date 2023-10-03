8e48d1ae7be3083e885b0f575559b81fa0ec4a0c
yoseph0524 <yoseph0524@g.ucla.edu>
2023-10-02 22:20:50 -0700

    clean code

--- libs/client/useMutation.tsx
+++ libs/client/useMutation.tsx
@@ -11,17 +11,6 @@ export default function useMutation(url: string): UseMutationResult {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<undefined | any>(undefined);
   const [error, setError] = useState<undefined | any>(undefined);
-  function mutation(data: any) {
-    setLoading(true);
-    fetch(url, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data),
-    })
-      .then((response) => response.json())
-      .then(setData)
-      .catch(setError)
-      .finally(() => setLoading(false));
-  }
+  function mutation(data: any) {}
   return [mutation, { loading, data, error }];
 }
