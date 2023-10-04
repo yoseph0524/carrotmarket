2fb62d4bdab79ea6f97fbffe8429a1c2a0fd218d
yoseph0524 <yoseph0524@g.ucla.edu>
2023-10-04 14:58:13 -0700

    useUser hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from "next";
 import FloatingButton from "@/components/floating-button";
 import Item from "@/components/item";
 import Layout from "@/components/layout";
+import useUser from "@/libs/client/useUser";
 
 const Home: NextPage = () => {
+  const user = useUser();
+  console.log(user);
+
   return (
     <Layout title="홈" hasTabBar>
       <div className="flex flex-col space-y-5 divide-y">
