09faa6d51e84189d524f1e46d88ba1ef1d1ea97e
yoseph0524 <yoseph0524@g.ucla.edu>
2023-10-20 21:49:27 -0700

    detailed get and post

--- pages/api/posts/[id].tsx
+++ pages/api/posts/[id].tsx
@@ -1,57 +1,60 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler, { ResponseType } from "@libs/server/withHandler";
-import client from "@libs/server/client";
-import { withApiSession } from "@libs/server/withSession";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
   const {
-    query: { id },
+    body: { question },
+    session: { user },
   } = req;
-  const post = await client.post.findUnique({
-    where: {
-      id: +id.toString(),
-    },
-    include: {
-      user: {
-        select: {
-          id: true,
-          name: true,
-          avatar: true,
-        },
-      },
-      answers: {
-        select: {
-          answer: true,
-          id: true,
-          user: {
-            select: {
-              id: true,
-              name: true,
-              avatar: true,
-            },
+  if (req.method === "POST") {
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
           },
         },
       },
-      _count: {
-        select: {
-          answers: true,
-          wondering: true,
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === "GET") {
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            avatar: true,
+          },
+        },
+        _count: {
+          select: {
+            wondering: true,
+            answers: true,
+          },
         },
       },
-    },
-  });
-  res.json({
-    ok: true,
-    post,
-  });
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
 }
 
 export default withApiSession(
   withHandler({
-    methods: ["GET"],
+    methods: ["GET", "POST"],
     handler,
   })
 );
