2e545ef65cdaa8720a88e3c369b1be432bd41bda
yoseph0524 <yoseph0524@g.ucla.edu>
2024-02-01 17:58:40 -0800

    auth css p1

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,5 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="bg-red-500">
-      <h1 className="text-black">It works</h1>
-    </div>
-  );
+  return null;
 }
