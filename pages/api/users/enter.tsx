783565fa431f7a1aa78f768da2c03b28961d74af
yoseph0524 <yoseph0524@g.ucla.edu>
2024-02-05 14:35:47 -0800

    twilio

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,64 +1,45 @@
 import client from "@/libs/server/client";
-import withHandler from "@/libs/server/withHandler";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { phone, email } = req.body;
-  const payload = phone ? { phone } : { email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const user = phone ? { phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
-
-  /*   if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email,
-      },
-    });
-    if (user) console.log("founded user");
-
-    if (!user) {
-      console.log("create");
-
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        },
-      });
-    }
-    console.log(user);
-  }
   if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone,
-      },
+    await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.PHONE_NUMBER!,
+      body: `Your login token is ${payload}.`,
     });
-    if (user) console.log("founded user");
+  }
 
-    if (!user) {
-      console.log("create");
-
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone,
-        },
-      });
-    }
-    console.log(user);
-  } */
-
-  return res.status(200).end();
+  return res.json({
+    ok: true,
+  });
 }
 
 export default withHandler("POST", handler);
