30bcc70e267fbcb5de9db4237f7946f0d3ea004f
yoseph0524 <yoseph0524@g.ucla.edu>
2024-02-04 23:04:41 -0800

    Enter form

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,37 +1,53 @@
+import type { NextPage } from "next";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Button from "../components/button";
+import Input from "../components/input";
+import { cls } from "../libs/utils";
 
-function cls(...classnames: string[]) {
-  return classnames.join(" ");
+interface EnterForm {
+  email?: string;
+  phone?: string;
 }
 
-export default function Enter() {
+const Enter: NextPage = () => {
+  const { register, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
-  const onEmailClick = () => setMethod("email");
-  const onPhoneClick = () => setMethod("phone");
+  const onEmailClick = () => {
+    reset();
+    setMethod("email");
+  };
+  const onPhoneClick = () => {
+    reset();
+    setMethod("phone");
+  };
+  const onValid = (data: EnterForm) => {
+    console.log(data);
+  };
   return (
     <div className="mt-16 px-4">
       <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
-      <div className="mt-8">
+      <div className="mt-12">
         <div className="flex flex-col items-center">
           <h5 className="text-sm text-gray-500 font-medium">Enter using:</h5>
-          <div className="grid border-b w-full mt-8 grid-cols-2 gap-16">
+          <div className="grid  border-b  w-full mt-8 grid-cols-2 ">
             <button
               className={cls(
-                "pb-4 font-medium border-b-2",
+                "pb-4 font-medium text-sm border-b-2",
                 method === "email"
-                  ? " border-orange-500 text-orange-500"
-                  : "border-transparent text-gray-500"
+                  ? " border-orange-500 text-orange-400"
+                  : "border-transparent hover:text-gray-400 text-gray-500"
               )}
               onClick={onEmailClick}
             >
-              Email Address
+              Email
             </button>
             <button
               className={cls(
-                "pb-4 font-medium border-b-2",
+                "pb-4 font-medium text-sm border-b-2",
                 method === "phone"
-                  ? " border-orange-500 text-orange-500"
-                  : "border-transparent text-gray-500"
+                  ? " border-orange-500 text-orange-400"
+                  : "border-transparent hover:text-gray-400 text-gray-500"
               )}
               onClick={onPhoneClick}
             >
@@ -39,47 +55,47 @@ export default function Enter() {
             </button>
           </div>
         </div>
-        <form className="flex flex-col mt-8">
-          <label className="text-sm font-medium text-gray-700">
-            {method === "email" ? "Email address" : null}
-            {method === "phone" ? "Phone number" : null}
-          </label>
-          <div className="mt-1">
-            {method === "email" ? (
-              <input
-                type="email"
-                className="appearance-none w-full px-3 border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                required
-              />
-            ) : null}
-            {method === "phone" ? (
-              <div className="flex rounded-md shadow-sm">
-                <span className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none">
-                  +82
-                </span>
-                <input
-                  className="appearance-none w-full px-3 border-gray-300 rounded-r-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-                  type="number"
-                  required
-                />
-              </div>
-            ) : null}
-          </div>
-          <button className=" mt-5 bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-400 focus:outline-none ">
-            {method === "email" ? "Get login link" : null}
-            {method === "phone" ? "Get one-time password" : null}
-          </button>
+        <form
+          onSubmit={handleSubmit(onValid)}
+          className="flex flex-col mt-8 space-y-4"
+        >
+          {method === "email" ? (
+            <Input
+              register={register("email", {
+                required: true,
+              })}
+              name="email"
+              label="Email address"
+              type="email"
+              required
+            />
+          ) : null}
+          {method === "phone" ? (
+            <Input
+              register={register("phone")}
+              name="phone"
+              label="Phone number"
+              type="number"
+              kind="phone"
+              required
+            />
+          ) : null}
+          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "phone" ? (
+            <Button text={"Get one-time password"} />
+          ) : null}
         </form>
+
         <div className="mt-8">
           <div className="relative">
             <div className="absolute w-full border-t border-gray-300" />
-            <div className="relative -top-3 text-center">
+            <div className="relative -top-3 text-center ">
               <span className="bg-white px-2 text-sm text-gray-500">
                 Or enter with
               </span>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-3 mt-2 ">
+          <div className="grid grid-cols-2 mt-2 gap-3">
             <button className="flex justify-center items-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50">
               <svg
                 className="w-5 h-5"
@@ -109,4 +125,5 @@ export default function Enter() {
       </div>
     </div>
   );
-}
+};
+export default Enter;
