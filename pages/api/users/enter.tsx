8afa4bd735bc69955ec9d9ed65df40a5fe9b06f0
yoseph0524 <yoseph0524@g.ucla.edu>
2024-02-05 13:35:51 -0800

    account logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,7 +3,61 @@ import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+
+  /*   if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (user) console.log("founded user");
+
+    if (!user) {
+      console.log("create");
+
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        },
+      });
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone,
+      },
+    });
+    if (user) console.log("founded user");
+
+    if (!user) {
+      console.log("create");
+
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone,
+        },
+      });
+    }
+    console.log(user);
+  } */
+
   return res.status(200).end();
 }
 
