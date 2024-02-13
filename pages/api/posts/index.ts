1583a5ec00f6bb0547d2cba7e52a08e007bfabce
yoseph0524 <yoseph0524@g.ucla.edu>
2024-02-12 22:58:48 -0800

    post and questions

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -7,13 +7,29 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const profile = await client.user.findUnique({
-    where: { id: req.session.user?.id },
+  const {
+    body: { question },
+    session: { user },
+  } = req;
+  const post = await client.post.create({
+    data: {
+      question,
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
   });
   res.json({
     ok: true,
-    profile,
+    post,
   });
 }
 
-export default withApiSession(withHandler({ methods: ["GET"], handler }));
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler,
+  })
+);
