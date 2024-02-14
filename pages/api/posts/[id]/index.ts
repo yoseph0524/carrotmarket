f5b9caa73d8d5a1aacf1f0bfb4fe880ab22baaaa
yoseph0524 <yoseph0524@g.ucla.edu>
2024-02-14 11:26:34 -0800

    community post

---
+++ pages/api/posts/[id]/index.ts
@@ -0,0 +1,64 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@/libs/server/withHandler";
+import client from "@/libs/server/client";
+import { withApiSession } from "@/libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+  if (id === undefined) {
+    // Handle the case where id is undefined, for example, return an error response
+    return res.status(400).json({
+      error: "ID is undefined",
+      ok: false,
+    });
+  }
+  const post = await client.post.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wondering: true,
+        },
+      },
+    },
+  });
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler,
+  })
+);
