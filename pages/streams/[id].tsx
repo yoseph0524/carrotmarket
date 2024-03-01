2645b9727c10cfcb92b4c2724cbcbb23833d6b2f
yoseph0524 <yoseph0524@g.ucla.edu>
2024-03-01 00:22:22 -0800

    mutations, seeding

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -35,7 +35,10 @@ const Stream: NextPage = () => {
   const { user } = useUser();
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data, mutate } = useSWR<StreamResponse>(
-    router.query.id ? `/api/streams/${router.query.id}` : null
+    router.query.id ? `/api/streams/${router.query.id}` : null,
+    {
+      refreshInterval: 1000,
+    }
   );
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${router.query.id}/messages`
@@ -43,13 +46,30 @@ const Stream: NextPage = () => {
   const onValid = (form: MessageForm) => {
     if (loading) return;
     reset();
+    mutate(
+      (prev) =>
+        prev &&
+        ({
+          ...prev,
+          stream: {
+            ...prev.stream,
+            messages: [
+              ...prev.stream.messages,
+              {
+                id: Date.now(),
+                message: form.message,
+                user: {
+                  ...user,
+                },
+              },
+            ],
+          },
+        } as any),
+      false
+    );
     sendMessage(form);
   };
-  useEffect(() => {
-    if (sendMessageData && sendMessageData.ok) {
-      mutate();
-    }
-  }, [sendMessageData, mutate]);
+
   return (
     <Layout canGoBack>
       <div className="py-10 px-4  space-y-4">
